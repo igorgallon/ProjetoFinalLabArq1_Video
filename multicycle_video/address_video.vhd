513a0c364bc2bd2558146dde7e509d8cef182bc9
Raphael Adamski <raphaeladamski@hotmail.com>
2014-12-09 18:51:57 -0200

    v1
    Primeira versao pronta com todas as funcoes, nao foram feitos testes
    praticos nem teoricos

--- multicycle_video/address_video.vhd
+++ multicycle_video/address_video.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 USE ieee.numeric_std.all;
 
 ENTITY address_video IS
@@ -18,15 +19,15 @@ ARCHITECTURE behavior OF address_video IS
 
 SIGNAL column_std: std_LOGIC_VECTOR(9 downto 0);
 signal pixel: std_logic;
-signal row_div2 : unsigned(11 downto 0);
+signal row_div16 : unsigned(11 downto 0);
 
 BEGIN
-  
+  -- formula pro endereco =TRUNCAR(A2/8)*10+TRUNCAR(B2/64)
   column_std <= std_logic_vector(to_unsigned(column, 10)); -- como nao cabe na memoria da fpga todos os pixels, fazemos uma gambiarra
-  row_div2 <= shift_right(to_unsigned(row, 12), 1); -- divide linha por 2, prenche um quadrado 2x2 para cada pixel
+  row_div16 <= shift_right(to_unsigned(row, 12), 4); -- divide linha por 2, depois por 8, prenche um quadrado 2x2 para cada pixel
   video_address <= std_logic_vector(
-              (shift_left(row_div2, 3)) -- como cada 10 palavras pintam uma linha (10palavras* 2colunas * 32bits)= 640,  fazemos row_div*8 + row_div*2
-            + (shift_left(row_div2, 1)) 
+              (shift_left(row_div16, 3)) -- como cada 10 palavras pintam uma linha (10palavras* 2colunas * 32bits)= 640,  fazemos row_div*8 + row_div*2
+            + (shift_left(row_div16, 1)) 
             + (shift_right(to_unsigned(column, 12), 6)));--como uma palavra pinta 64 colunas, fazemos divisao por 64
   
   pixel <= video_out(31-to_integer(unsigned(column_std(5 downto 1)))); -- pintana tela 0x0 a mposicao mais significativa da word
--- multicycle_video/video_decoder.vhdl
+++ multicycle_video/video_decoder.vhdl
@@ -5,12 +5,13 @@ use ieee.numeric_std.all;
 
 entity video_decoder is
 	generic (
-		address_width: integer := 9;
+		address_width: integer := 6;
 		data_width: integer := 32;
 		rom_width: integer := 8);
 
 	port (
     clock: std_logic;
+	 row          : IN  INTEGER;
 		video_out: in std_logic_vector (data_width - 1 downto 0);
 		modified_video_out: out std_logic_vector(data_width-1 downto 0));
 end video_decoder;
@@ -55,24 +56,42 @@ type data_sequence is array (0 to address_width-1) of std_logic_vector (rom_widt
 --signal contador: std_logic_vector(3 downto 0);
 --signal std_logic_vector (data_width - 1 downto 0);
 constant zeroes: std_logic_vector(data_width - 1 downto 0) := (others => '0');
+signal row_div2 : unsigned(11 downto 0);
 
 
 begin
    
+	row_div2 <= shift_right(to_unsigned(row, 12), 1);
+	
  	read_video: process (clock)
-		variable index: integer;
-		variable coluna: integer:=9;
-		variable linha: integer:=0;
+		variable char1: integer;
+		variable char2: integer;
+		variable char3: integer;
+		variable char0: integer;
+		--variable row_div2: unsigned;
+--		variable coluna: integer:=9;
+--		variable linha: integer:=0;
 	begin
+	
    if(rising_edge(clock)) then 
-			if(coluna = 9) then
-				index := to_integer(unsigned(video_out));
-				modified_video_out <= ( zeroes(31 downto 8)	& data(index + linha)(7 downto 0)) ;
-				linha := linha+1;
-				coluna :=0;
-			else
-				coluna := coluna + 1 ;
-			end if;
+			char0 := to_integer((shift_right(unsigned(video_out), 24 ))) + to_integer(row_div2);
+			char1 := to_integer((shift_right(unsigned(video_out), 16 )))+ to_integer(row_div2);
+			char2 := to_integer((shift_right(unsigned(video_out), 8 )))+ to_integer(row_div2);
+			char3 :=  to_integer(unsigned(video_out))+ to_integer(row_div2);
+			modified_video_out <= ( data(char0)(rom_width-1 downto 0)
+											& data(char1)(rom_width-1 downto 0)
+											& data(char2)(rom_width-1 downto 0)
+											& data(char3)(rom_width-1 downto 0)) ;
+			
+--			if(coluna = 9) then
+--				index := to_integer(unsigned(video_out));
+--				modified_video_out <= ( zeroes(31 downto 8)	& data(index)(7 downto 0)) ;
+--				linha := linha+1;
+--				coluna :=0;
+--			else
+--				coluna := coluna + 1 ;
+--			end if;
+
    end if;
 	end process;
 
