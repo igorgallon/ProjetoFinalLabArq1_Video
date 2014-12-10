cff5befcd0f8935f921d4e8145bb19b123fa724d
Igor Felipe Gallon <igor.felipe.gallon@outlook.com>
2014-12-10 21:56:08 -0200

    Revert "Revert "v1.1""
    This reverts commit 2e43d09dfe4ab411a1913ff502e06d9223b02f25.

--- multicycle_video/video_decoder.vhdl
+++ multicycle_video/video_decoder.vhdl
@@ -11,6 +11,7 @@ entity video_decoder is
 
 	port (
     clock: std_logic;
+	 row          : IN  INTEGER;
 		video_out: in std_logic_vector (data_width - 1 downto 0);
 		modified_video_out: out std_logic_vector(data_width-1 downto 0));
 end video_decoder;
@@ -55,24 +56,53 @@ type data_sequence is array (0 to address_width-1) of std_logic_vector (rom_widt
 --signal contador: std_logic_vector(3 downto 0);
 --signal std_logic_vector (data_width - 1 downto 0);
 constant zeroes: std_logic_vector(data_width - 1 downto 0) := (others => '0');
+signal row_div2 : unsigned(2 downto 0);
 
 
 begin
-   
+   --(row mod 2) div 8
+	row_div2 <= shift_right(to_unsigned(row, 4), 1)(2 downto 0);
+	
  	read_video: process (clock)
+		variable char1: integer;
+		variable char2: integer;
+		variable char3: integer;
+		variable char0: integer;
 		variable index: integer;
-		variable coluna: integer:=9;
-		variable linha: integer:=0;
+		--variable row_div2: unsigned;
+--		variable coluna: integer:=9;
+--		variable linha: integer:=0;
 	begin
+	
    if(rising_edge(clock)) then 
-			if(coluna = 9) then
-				index := to_integer(unsigned(video_out));
-				modified_video_out <= ( zeroes(31 downto 8)	& data(index + linha)(7 downto 0)) ;
-				linha := linha+1;
-				coluna :=0;
-			else
-				coluna := coluna + 1 ;
-			end if;
+			
+			char0 := to_integer(shift_left(unsigned(video_out( 7 downto  0)),0));
+			char1 := to_integer(shift_left(unsigned(video_out(15 downto  8)),0));
+			char2 := to_integer(shift_left(unsigned(video_out(23 downto  16)),3));
+			char3 := to_integer(shift_left(unsigned(video_out(31 downto  24)),3));
+			modified_video_out <= ( zeroes(31 downto 16)	--& data(char3 + to_integer(row_div2))
+																		--& data(char2 + to_integer(row_div2))
+																		& data(char1 + to_integer(row_div2))
+																		& data(char0 + to_integer(row_div2))) ;
+	
+--			char0 := to_integer(unsigned(video_out(31 downto 24)))*10+ to_integer(row_div2);
+--			char1 := to_integer(unsigned(video_out(23 downto 16)))*10+ to_integer(row_div2);
+--			char2 := to_integer(unsigned(video_out(15 downto  8)))*10+ to_integer(row_div2);
+--			char3 := to_integer(unsigned(video_out( 7 downto  0)))*10+ to_integer(row_div2);
+--			modified_video_out <= ( data(char0)(rom_width-1 downto 0)
+--											& data(char1)(rom_width-1 downto 0)
+--											& data(char2)(rom_width-1 downto 0)
+--											& data(char3)(rom_width-1 downto 0)) ;
+			
+--			if(coluna = 9) then
+--				index := to_integer(unsigned(video_out));
+--				modified_video_out <= ( zeroes(31 downto 8)	& data(index)(7 downto 0)) ;
+--				linha := linha+1;
+--				coluna :=0;
+--			else
+--				coluna := coluna + 1 ;
+--			end if;
+
    end if;
 	end process;
 
